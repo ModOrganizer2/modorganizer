ec07d3d4d9ec2245f0b23edf6746f4ae1e5dfce4
Thomas Tanner <trtanner@btinternet.com>
2015-10-13 15:24:41 +0100

    Updated scons build a bit to produce helpful-ish git tag for organizer.
    Use r"..." strings in template file to make it easier to copy/paste paths from windows clipboard.
    
    Changed organizer.pro to not produce strange messages if you opened it because it couldn't find a .hg directory

--- scons_configure_template.py
+++ scons_configure_template.py
@@ -4,7 +4,7 @@
 # Path to your boost install - it should have a boost/ subdirectory and a stage/
 # subdirectory. The scons script will use stage/lib if there, or the appropriate
 # version for your compiler, if you installed the multiple-build version
-BOOSTPATH = 'C:\\Apps\\boost_1_55_0'
+BOOSTPATH = r"C:\Apps\boost_1_55_0"
 
 # Version of Visual Studio to use, if you wish to use a specific version. If you
 # don't specify a version, the latest will be picked.. See the scons manual for
@@ -14,15 +14,20 @@ BOOSTPATH = 'C:\\Apps\\boost_1_55_0'
 # Path to your python install
 # You don't really need to set this up but you might if (say) you have a 32- and
 # 64-bit python install and scons has been installed for the 64 bit version
-#PYTHONPATH=C:\Apps\Python
+#PYTHONPATH=r"C:\Apps\Python"
 
 # Path to your QT install. This might constrain the version of MSVC you can use.
 # This seems to be set by QTCreator
-#QTDIR = 'C:\\Apps\\Qt\\4.8.6'
+#QTDIR = r"C:\Apps\Qt\4.8.6"
 
 # Path to 7-zip sources
-SEVENZIPPATH = 'C:\\Apps\\7-Zip\\7z920'
+SEVENZIPPATH = r"C:\Apps\7-Zip\7z920"
 
 # Path to zlib. Please read the README file for more information about how this
 # needs to be set up
-ZLIBPATH = 'C:\\Apps\\zlib-1.2.8'
+ZLIBPATH = r"C:\Apps\zlib-1.2.8"
+
+# Source control programs. Sadly I can't get this information from qt, even
+# though you have to set it up in the configuration
+GIT = r"C:\Program Files\git\bin\git.exe"
+MERCURIAL = r"C:\Program Files\TortoiseHg\hg.exe"
