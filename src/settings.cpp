7b087f8f9d94b3758b3dad8d29670cd73d058510
Thomas Tanner <trtanner@btinternet.com>
2015-12-06 21:10:37 +0000

    Facepalm moments - realised gameinfo init call is no longer needed. Removed all references

--- src/settings.cpp
+++ src/settings.cpp
@@ -22,7 +22,6 @@ along with Mod Organizer.  If not, see <http://www.gnu.org/licenses/>.
 #include "settingsdialog.h"
 #include "utility.h"
 #include "helper.h"
-#include <gameinfo.h>
 #include <appconfig.h>
 #include <utility.h>
 #include <iplugingame.h>
