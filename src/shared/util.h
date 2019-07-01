ffebd4c1016265eeebab67d7f22d8f5bfd67703e
isanae <14251494+isanae@users.noreply.github.com>
2019-07-01 14:51:08 -0400

    a few more comments

--- src/shared/util.h
+++ src/shared/util.h
@@ -241,6 +241,8 @@ private:
 };
 
 
+// represents a security product, such as an antivirus or a firewall
+//
 class SecurityProduct
 {
 public:
@@ -248,11 +250,24 @@ public:
     QString name, int provider,
     bool active, bool upToDate);
 
+  // display name of the product
+  //
   const QString& name() const;
+
+  // a bunch of _WSC_SECURITY_PROVIDER flags
+  //
   int provider() const;
+
+  // whether the product is active
+  //
   bool active() const;
+
+  // whether its definitions are up-to-date
+  //
   bool upToDate() const;
 
+  // string representation of the above
+  //
   QString toString() const;
 
 private:
