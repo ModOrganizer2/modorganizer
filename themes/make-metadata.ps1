30e0354aa42861720f7415cbd3fb051b12baba72
Mikaël Capelle <capelle.mikael@gmail.com>
2023-07-22 14:39:16 +0200

    Update theme extensions.

--- themes/make-metadata.ps1
+++ themes/make-metadata.ps1
@@ -1,25 +1,46 @@
-$fixNames = @{
-    dark      = "Dark";
-    dracula   = "Dracula";
-    nighteyes = "Night Eyes";
-    parchment = "Parchment";
-    skyrim    = "Skyrim";
-}
-
-Get-ChildItem -Directory -Exclude "vs15" | ForEach-Object {
-    $name = $fixNames[$_.Name];
+Get-ChildItem -Directory | ForEach-Object {
+    $metadata = Get-Content "$_/metadata.json" | ConvertFrom-Json
+    $metadata
+    $name = $metadata.identifier -replace "-themes?"
     $data = [ordered]@{
-        identifier  = $_.Name + "-theme";
+        id          = "mo2-theme-" + $name;
         type        = "theme";
-        name        = "$name Theme";
-        description = "$name theme for ModOrganizer2.";
+        name        = $metadata.name;
+        description = $metadata.description;
         version     = "1.0.0";
-        themes      = @{
-            $_.Name = [ordered]@{
-                name = $name;
-                path = (Get-ChildItem $_ -Filter "*.qss")[0].Name;
-            }
+        content     = @{
+            themes = $metadata.themes;
         };
     }
-    ConvertTo-Json -InputObject $data | Set-Content (Join-Path $_ "metadata.json")
+    $data
+    # ConvertTo-Json -InputObject $data #  | Set-Content (Join-Path $_ "metadata.json")
 }
+
+# $fixNames = @{
+#     dark      = "Dark";
+#     dracula   = "Dracula";
+#     nighteyes = "Night Eyes";
+#     parchment = "Parchment";
+#     skyrim    = "Skyrim";
+# }
+
+# Get-ChildItem -Directory -Exclude "vs15" | ForEach-Object {
+#     $name = $fixNames[$_.Name];
+#     $data = [ordered]@{
+#         identifier  = "mo2-theme-" + $_.Name;
+#         type        = "theme";
+#         name        = "$name Theme";
+#         description = "$name theme for ModOrganizer2.";
+#         version     = "1.0.0";
+#         content     = @{
+#             themes = @{
+#                 $_.Name = [ordered]@{
+#                     name = $name;
+#                     path = (Get-ChildItem $_ -Filter "*.qss")[0].Name;
+#                 }
+#             };
+#         };
+#         ;
+#     }
+#     ConvertTo-Json -InputObject $data | Set-Content (Join-Path $_ "metadata.json")
+# }
