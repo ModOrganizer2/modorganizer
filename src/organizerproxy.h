8f61772da051c6c426cf0aa683a110fff94f447f
Mikaël Capelle <capelle.mikael@gmail.com>
2024-06-30 15:18:45 +0200

    Switch VersionInfo -> Version for ModOrganizer2.

--- src/organizerproxy.h
+++ src/organizerproxy.h
@@ -29,69 +29,67 @@ public:
   MOBase::IPlugin* plugin() const { return m_Plugin; }
 
 public:  // IOrganizer interface
-  virtual MOBase::IModRepositoryBridge* createNexusBridge() const;
-  virtual QString profileName() const;
-  virtual QString profilePath() const;
-  virtual QString downloadsPath() const;
-  virtual QString overwritePath() const;
-  virtual QString basePath() const;
-  virtual QString modsPath() const;
-  virtual MOBase::VersionInfo appVersion() const;
-  virtual MOBase::IPluginGame* getGame(const QString& gameName) const;
-  virtual MOBase::IModInterface* createMod(MOBase::GuessedValue<QString>& name);
-  virtual void modDataChanged(MOBase::IModInterface* mod);
-  virtual QVariant persistent(const QString& pluginName, const QString& key,
-                              const QVariant& def = QVariant()) const;
-  virtual void setPersistent(const QString& pluginName, const QString& key,
-                             const QVariant& value, bool sync = true);
-  virtual QString pluginDataPath() const;
-  virtual MOBase::IModInterface* installMod(const QString& fileName,
-                                            const QString& nameSuggestion = QString());
-  virtual QString resolvePath(const QString& fileName) const;
-  virtual QStringList listDirectories(const QString& directoryName) const;
-  virtual QStringList
+  MOBase::IModRepositoryBridge* createNexusBridge() const override;
+  QString profileName() const override;
+  QString profilePath() const override;
+  QString downloadsPath() const override;
+  QString overwritePath() const override;
+  QString basePath() const override;
+  QString modsPath() const override;
+  MOBase::Version version() const override;
+  MOBase::VersionInfo appVersion() const override;
+  MOBase::IPluginGame* getGame(const QString& gameName) const override;
+  MOBase::IModInterface* createMod(MOBase::GuessedValue<QString>& name) override;
+  void modDataChanged(MOBase::IModInterface* mod) override;
+  QVariant persistent(const QString& pluginName, const QString& key,
+                      const QVariant& def = QVariant()) const override;
+  void setPersistent(const QString& pluginName, const QString& key,
+                     const QVariant& value, bool sync = true) override;
+  QString pluginDataPath() const override;
+  MOBase::IModInterface* installMod(const QString& fileName,
+                                    const QString& nameSuggestion = QString());
+  QString resolvePath(const QString& fileName) const override;
+  QStringList listDirectories(const QString& directoryName) const override;
+  QStringList
   findFiles(const QString& path,
             const std::function<bool(const QString&)>& filter) const override;
-  virtual QStringList findFiles(const QString& path,
-                                const QStringList& globFilters) const override;
-  virtual QStringList getFileOrigins(const QString& fileName) const override;
-  virtual QList<FileInfo>
+  QStringList findFiles(const QString& path,
+                        const QStringList& globFilters) const override;
+  QStringList getFileOrigins(const QString& fileName) const override;
+  QList<FileInfo>
   findFileInfos(const QString& path,
                 const std::function<bool(const FileInfo&)>& filter) const override;
-  virtual std::shared_ptr<const MOBase::IFileTree> virtualFileTree() const override;
+  std::shared_ptr<const MOBase::IFileTree> virtualFileTree() const override;
 
-  virtual MOBase::IDownloadManager* downloadManager() const override;
-  virtual MOBase::IPluginList* pluginList() const override;
-  virtual MOBase::IModList* modList() const override;
-  virtual MOBase::IProfile* profile() const override;
-  virtual MOBase::IGameFeatures* gameFeatures() const override;
+  MOBase::IDownloadManager* downloadManager() const override;
+  MOBase::IPluginList* pluginList() const override;
+  MOBase::IModList* modList() const override;
+  MOBase::IProfile* profile() const override;
+  MOBase::IGameFeatures* gameFeatures() const override;
 
-  virtual HANDLE startApplication(const QString& executable,
-                                  const QStringList& args = QStringList(),
-                                  const QString& cwd = "", const QString& profile = "",
-                                  const QString& forcedCustomOverwrite = "",
-                                  bool ignoreCustomOverwrite           = false);
-  virtual bool waitForApplication(HANDLE handle, bool refresh = true,
-                                  LPDWORD exitCode = nullptr) const;
-  virtual void refresh(bool saveChanges);
+  HANDLE startApplication(const QString& executable,
+                          const QStringList& args = QStringList(),
+                          const QString& cwd = "", const QString& profile = "",
+                          const QString& forcedCustomOverwrite = "",
+                          bool ignoreCustomOverwrite           = false) override;
+  bool waitForApplication(HANDLE handle, bool refresh = true,
+                          LPDWORD exitCode = nullptr) const override;
+  void refresh(bool saveChanges) override;
 
-  virtual bool onAboutToRun(const std::function<bool(const QString&)>& func) override;
-  virtual bool onAboutToRun(const std::function<bool(const QString&, const QDir&,
-                                                     const QString&)>& func) override;
-  virtual bool
+  bool onAboutToRun(const std::function<bool(const QString&)>& func) override;
+  bool onAboutToRun(const std::function<bool(const QString&, const QDir&,
+                                             const QString&)>& func) override;
+  bool
   onFinishedRun(const std::function<void(const QString&, unsigned int)>& func) override;
-  virtual bool
+  bool
   onUserInterfaceInitialized(std::function<void(QMainWindow*)> const& func) override;
-  virtual bool onNextRefresh(const std::function<void()>& func,
-                             bool immediateIfPossible) override;
-  virtual bool
-  onProfileCreated(std::function<void(MOBase::IProfile*)> const& func) override;
-  virtual bool onProfileRenamed(
-      std::function<void(MOBase::IProfile*, QString const&, QString const&)> const&
-          func) override;
-  virtual bool
-  onProfileRemoved(std::function<void(QString const&)> const& func) override;
-  virtual bool onProfileChanged(
+  bool onNextRefresh(const std::function<void()>& func,
+                     bool immediateIfPossible) override;
+  bool onProfileCreated(std::function<void(MOBase::IProfile*)> const& func) override;
+  bool onProfileRenamed(std::function<void(MOBase::IProfile*, QString const&,
+                                           QString const&)> const& func) override;
+  bool onProfileRemoved(std::function<void(QString const&)> const& func) override;
+  bool onProfileChanged(
       std::function<void(MOBase::IProfile*, MOBase::IProfile*)> const& func) override;
 
   // Plugin related:
