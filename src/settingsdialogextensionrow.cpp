5061ff5e59640fc4c6bf2a41586038221a5997ac
Mikaël Capelle <capelle.mikael@gmail.com>
2024-08-09 15:12:49 +0200

    Default icon for extension.

--- src/settingsdialogextensionrow.cpp
+++ src/settingsdialogextensionrow.cpp
@@ -4,13 +4,46 @@
 
 using namespace MOBase;
 
+namespace
+{
+
+const auto& defaultIcon()
+{
+  static QIcon icon;
+
+  if (icon.isNull()) {
+    const QImage baseIcon(":/MO/gui/app_icon");
+    QImage grayIcon = baseIcon.convertToFormat(QImage::Format_ARGB32);
+    {
+      for (int y = 0; y < grayIcon.height(); ++y) {
+        QRgb* scanLine = (QRgb*)grayIcon.scanLine(y);
+        for (int x = 0; x < grayIcon.width(); ++x) {
+          QRgb pixel = *scanLine;
+          uint ci    = uint(qGray(pixel));
+          *scanLine  = qRgba(ci, ci, ci, qAlpha(pixel) / 3);
+          ++scanLine;
+        }
+      }
+    }
+    icon = QIcon(QPixmap::fromImage(grayIcon));
+  }
+
+  return icon;
+}
+
+}  // namespace
+
 ExtensionListItemWidget::ExtensionListItemWidget(const IExtension& extension)
     : ui{new Ui::ExtensionListItemWidget()}, m_extension{&extension}
 {
   ui->setupUi(this);
 
+  const auto& metadata = extension.metadata();
+
+  const auto& icon = metadata.icon().isNull() ? defaultIcon() : metadata.icon();
+
   QIcon icon = style()->standardIcon(QStyle::SP_DialogOkButton);
-  ui->extensionIcon->setPixmap(extension.metadata().icon().pixmap(QSize(48, 48)));
+  ui->extensionIcon->setPixmap(icon.pixmap(QSize(48, 48)));
   ui->extensionName->setText(extension.metadata().name());
 
   ui->extensionDescription->setText(extension.metadata().description());
