8016e77723b7de88a3900ad3f47efc73edc66fae
itch <75189058+DLST316@users.noreply.github.com>
2025-09-15 16:40:50 +0900

    fix(qt): remove obsolete call to AA_EnableHighDpiScaling (#2283)

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,6 @@ int run(int argc, char* argv[])
   // must be after logging
   TimeThis tt("main() multiprocess");
 
-  QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
   MOApplication app(argc, argv);
 
   // check if the command line wants to run something right now
